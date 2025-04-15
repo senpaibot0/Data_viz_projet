1932df4ef4024461cef9ef61d23505c2c73230ca
Anouar <hannachianouar5588@gmail.com>
2025-04-15 14:49:52 -0400

    changement du port de server.py

--- src/server.py
+++ src/server.py
@@ -1,24 +1,18 @@
 '''
     Contains the server to run our application.
 '''
-# from flask_failsafe import failsafe
+from flask_failsafe import failsafe
 
-# @failsafe
-# def create_app():
-#     '''
-#         Gets the underlying Flask server from our Dash app.
-#     '''
-#     from app import app  # pylint: disable=import-outside-toplevel
-#     return app.server
-from app import app
+@failsafe
+def create_app():
+    '''
+        Gets the underlying Flask server from our Dash app.
+    '''
+    from app import app  # pylint: disable=import-outside-toplevel
+    return app.server
 
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 10000))
-    app.server.run(host="0.0.0.0", port=port, debug=True)
-
-# if __name__ == "__main__":
-#     import os
-#     port = int(os.environ.get("PORT", 10000))
-#     print(f"✅ Starting server on port {port}")
-#     create_app().run(host="0.0.0.0", port=port, debug=True)
+    print(f"✅ Starting server on port {port}")
+    create_app().run(host="0.0.0.0", port=port, debug=True)
